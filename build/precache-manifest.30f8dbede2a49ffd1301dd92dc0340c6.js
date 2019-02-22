faf1d46412f0488c536cf5ce5edcb51da2b6658f
Jacky Fakkeldij <jackyfakkeldij@hotmail.com>
2019-02-22 20:35:58 +0100

    first

---
+++ build/precache-manifest.30f8dbede2a49ffd1301dd92dc0340c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "21b6bb239a4329c437e3",
+    "url": "/static/js/main.21b6bb23.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "21b6bb239a4329c437e3",
+    "url": "/static/css/main.05cdab41.chunk.css"
+  },
+  {
+    "revision": "07f71bcf72b1b1034534c6f70f78852a",
+    "url": "/index.html"
+  }
+];
